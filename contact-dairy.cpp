1abfc4ee2d7eedff407a489473d5531d53113c5b
unknown <bossrc64@outlook.com>
2022-10-10 16:13:55 +0530

    Third commit

--- contact-dairy.cpp
+++ contact-dairy.cpp
@@ -9,6 +9,10 @@ class DairyTemplate {
     long long phno;
 
     public:
+
+    // DairyTemplate() {
+
+    // }
     DairyTemplate(std::string fname, std::string lname, std::string mail, long long phnum) {
         f_name = fname;
         l_name = lname;
@@ -16,29 +20,40 @@ class DairyTemplate {
         phno = phnum;
     }
 
-    std::string getFirstname(){
-        return f_name;
-    }
-    std::string getLastName(){
-        return l_name;
-    }
-    std::string getFullName(){
-        return f_name + ' ' + l_name;
-    }
+    std::string getFirstname() { return f_name;  }
+    
+    std::string getLastName() {  return l_name; }
+    
+    std::string getFullName() {  return f_name + ' ' + l_name; }
 
-    long long getPhoneNumber() {
-        return phno;
-    }
+    long long getPhoneNumber() {    return phno;   }
 
 };
 
-// class ContactDairy {
-//     DairyTemplate d1 {"Rahul", " Chaudhary", "rahulchy.rc64@gmail.com", 9135811774};
-//     std::vector<DairyTemplate> contactDairy{d1};
-// };
+class ContactDairy {
+
+    public:
+    void addContact() {
+        std::cout<<"Adding contact... \n";
+    }
+    void searchContact() {
+        std::cout<<"Searching contact... \n";
+    }
+    void modifyContact() {
+        std::cout<<"Modifying contact... \n";
+    }
+    void removeContact() {
+        std::cout<<"Removing contact... \n";
+    } 
+};
 
 int main() {
 
+    std::vector<DairyTemplate> d{
+        {"Rahul", "Chaudhary", "rahulchy.rc64@gmail.com", 9135811774},
+        {"Sahil", "Chaudhary", "sahilchaudhary@gmail.com", 7004013713},
+    };
+    ContactDairy dc;
     std::cout<< "Select an option: \n";
     std::cout<< "1. Add a contact: \n";
     std::cout<< "2. search a contact: \n";
@@ -49,28 +64,23 @@ int main() {
     std::cin>>inp;
     std::cout<<'\n';
     
-    DairyTemplate dt;
+    
     if(inp == 1) {
-        dt.addContact();
+        dc.addContact();
     }
     else if(inp == 2) {
-        dt.SearchContact();
+        dc.searchContact();
     }
     else if(inp == 3) {
-        dt.ModifyContact();
+        dc.modifyContact();
     }
     else if(inp == 4) {
-        dt.RemoveContact();
+        dc.removeContact();
     }
     else {
         std::cout<<"*Error*\nEnter a valid option.\n";
     }
-    std::vector<DairyTemplate> d{
-        {"Rahul", "Chaudhary", "rahulchy.rc64@gmail.com", 9135811774},
-        {"Sahil", "Chaudhary", "sahilchaudhary@gmail.com", 7004013713},
-    };
-
-
+    
 
     return 0;
 }
